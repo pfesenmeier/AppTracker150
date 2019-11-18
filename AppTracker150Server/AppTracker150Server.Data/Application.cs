a278664752eaca04eac073f60d8acc8cce3e1156
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-18 14:52:56 -0500

    ApplicationService

---
+++ AppTracker150Server/AppTracker150Server.Data/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTracker150Server.Data
+{
+    public enum ApplicationStatus { Hired, Rejected, Pending }
+    public class Application
+    {
+        [Required]
+        public int Id { get; set; }
+        public Guid StudentId { get; set; }
+        public DateTimeOffset DateCreatedUtc { get; set; }
+        public DateTimeOffset? DateModifiedUtc { get; set; }
+        public ApplicationStatus ApplicationStatus { get; set; }
+        public string CompanyName { get; set; }
+        public string PositionName { get; set; }
+        public string JobLink { get; set; }
+        public string JobLocation { get; set; }
+        public string Research { get; set; }
+        public string Contacts { get; set; }
+        public string SourceOfPosting { get; set; }
+    }
+}
--- AppTracker150Server/AppTracker150Server.Data/IdentityModels.cs
+++ AppTracker150Server/AppTracker150Server.Data/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -23,7 +24,9 @@ namespace AppTracker150Server.Data
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
+        public DbSet<Application> Applications { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
