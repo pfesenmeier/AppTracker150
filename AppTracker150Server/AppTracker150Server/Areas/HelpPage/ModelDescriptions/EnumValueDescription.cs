d5ec567b49bb9ee8cf33a6dffc65b4c6997e345e
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-18 12:09:16 -0500

    scaffold, ignore files

---
+++ AppTracker150Server/AppTracker150Server/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace AppTracker150Server.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
