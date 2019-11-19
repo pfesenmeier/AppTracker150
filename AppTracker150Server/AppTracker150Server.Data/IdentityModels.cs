ab994f08eea3ec771b0838163c99e8c28294bada
Aaron Floyd <aaronrfloyd@gmail.com>
2019-11-19 10:07:00 -0500

    branchfix

--- AppTracker150Server/AppTracker150Server.Data/IdentityModels.cs
+++ AppTracker150Server/AppTracker150Server.Data/IdentityModels.cs
@@ -26,10 +26,14 @@ namespace AppTracker150Server.Data
         }
 
         public DbSet<Application> Applications { get; set; }
+        public DbSet<Cohort> Cohorts { get; set; }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+
+
+     
     }
 }
