2e8326acf700cbceec9de2a83271f3e34048d1f8
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-22 13:56:28 -0500

    cors

--- AppTracker150Server/AppTracker150Server/App_Start/WebApiConfig.cs
+++ AppTracker150Server/AppTracker150Server/App_Start/WebApiConfig.cs
@@ -17,6 +17,7 @@ namespace AppTracker150Server
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
